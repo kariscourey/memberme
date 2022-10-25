05ad1111a6067dabf4223107609bc66a7c25d508
Angel Sierra <asierra2002@yahoo.com>
2022-10-25 14:59:21 -0400

    new commit

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length=200, unique=True, null=True)
     color = models.CharField(max_length=50)
     year = models.PositiveSmallIntegerField()
     vin = models.CharField(max_length=17, unique=True)
