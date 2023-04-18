81730b153fb0ba65fe0fd392f2b97d90b92e1a81
karis courey <kariscourey@gmail.com>
2023-04-18 00:29:51 -0400

    update package

--- ghi/app/run.sh
+++ ghi/app/run.sh
@@ -1,6 +1,7 @@
 npm install
 npm install @mui/material @emotion/react @emotion/styled
 npm install @mui/icons-material
+npm install react-router-dom
 npm install @reduxjs/toolkit
 npm install @reduxjs/toolkit react-redux
 npm start
